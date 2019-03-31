13976b2fd954d476ab0245b3ae4d5a569434ca71
Sajindra Prasad <Sajindra Prasad@192.168.0.114>
2019-03-31 12:51:46 +0530

    cc

--- src/test/java/AppiumTest/com/appium/AppTest.java
+++ src/test/java/AppiumTest/com/appium/AppTest.java
@@ -15,9 +15,9 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public AppTest( String testNames )
     {
-        super( testName );
+        super( testNames );
     }
 
     /**
