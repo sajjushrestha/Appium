3753e8dc6c097068e439ab47ab573a44780b41a6
Sajindra Prasad <Sajindra Prasad@192.168.0.116>
2019-04-06 23:54:08 +0530

    connected with mobile device

--- src/test/java/AppiumTest/com/appium/Calci.java
+++ src/test/java/AppiumTest/com/appium/Calci.java
@@ -3,6 +3,7 @@ package AppiumTest.com.appium;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
@@ -20,20 +21,20 @@ public class Calci {
 	{
 		DesiredCapabilities dc=new DesiredCapabilities();
 		
-		dc.setCapability("deviceName", "Moto G5 Plus");
-		dc.setCapability("udid", "ZY223Z9PHM");
+		dc.setCapability("deviceName", "MI A2");
+		dc.setCapability("udid", "4b18ae7");
 		dc.setCapability("platformName", "Android");
-		dc.setCapability("platformVersion", "7.0");
+		dc.setCapability("platformVersion", "9.0");
 		
-		dc.setCapability("appPackage", "com.google.android.calculator");
-		dc.setCapability("appActivity", "com.android.calculator2.Calculator");
+		dc.setCapability("appPackage", "com.opera.mini.native");
+		dc.setCapability("appActivity", "com.opera.mini.android.Browser");
 		
 		driver=new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),dc);
 		
+		//driver.findElement(By.xpath("//android.widget.Button[@text='1' and @index='6']")).click();
+		//driver.findElement(By.xpath("//android.view.ViewGroup[@index='1']")).click();
 		
-		
-		
-		
+		//driver.findElement(By.xpath("//android.widget.Button[@text='2']")).click();
 		
 	}
 	
