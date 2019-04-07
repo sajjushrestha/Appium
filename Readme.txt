224f7d5327fe60de48804d2425f3401a9f3a9bcd
Sajindra Prasad <Sajindra Prasad@192.168.0.116>
2019-04-07 17:22:20 +0530

    added code to auto start appium server

---
+++ Readme.txt
@@ -0,0 +1,8 @@
+From cmd:
+adb devices -- to know the devices connected to system
+
+to get platform tools -open cmd tools\bin and type cmd in address bar - sdkmanager "platform-tools" "platforms:android-26
+
+To install appium server through npm : 
+node.js should be installed before this
+npm install -g appium
--- src/test/java/AppiumTest/com/appium/Calci.java
+++ src/test/java/AppiumTest/com/appium/Calci.java
@@ -10,6 +10,7 @@ import org.testng.annotations.Test;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
 
 public class Calci {
 
@@ -19,6 +20,10 @@ public class Calci {
 	
 	public void launchCalci() throws Exception
 	{
+		AppiumDriverLocalService service = AppiumDriverLocalService.buildDefaultService();
+		service.start();
+		
+		
 		DesiredCapabilities dc=new DesiredCapabilities();
 		
 		dc.setCapability("deviceName", "MI A2");
@@ -35,6 +40,9 @@ public class Calci {
 		//driver.findElement(By.xpath("//android.view.ViewGroup[@index='1']")).click();
 		
 		//driver.findElement(By.xpath("//android.widget.Button[@text='2']")).click();
+		
+		
+		service.stop();
 		
 	}
 	
